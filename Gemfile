11540aa1ef26d1c2b35f3664537515310dcf03a8
JD Frag <jdfrag@mail.ru>
2011-09-07 20:36:43 +0300

    приложение и features

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'pg'
+gem "devise"
+gem "paperclip"
+gem "jquery-rails"
+
+group :test,:development do
+  gem "parallel_tests"
+  gem "headless"
+  gem "factory_girl_rails"
+  gem "pickle"
+  gem 'email_spec'
+  gem "wirble"
+  gem 'Selenium',            '1.1.14'
+  gem "database_cleaner"
+  gem "capybara"
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem "builder", "2.1.2"
+  gem "awesome_print"
+  gem "looksee"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studyfiles::Application.config.secret_token = '0065239c9cb933df47c057e36a2041c65d167fa73f3ef9e86f2467d3805afb40425c9c9833990f75e1b16396fc5e4225959b9babeae3290a4babf1a8f3f33e14'
