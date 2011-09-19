55bf6d5f79d0eab8be9c7d620b631a7dc5c0e979
dima <pronix.service@gmail.com>
2011-09-19 15:43:10 +0400

    require fix

--- db/migrate/20110914103249_add_ltree_type.rb
+++ db/migrate/20110914103249_add_ltree_type.rb
@@ -1,5 +1,6 @@
 class AddLtreeType < ActiveRecord::Migration
   def self.up
+    #FIXME user require load from config
     cmd = "psql -Upostgres -f `pg_config --sharedir`/contrib/ltree.sql #{ActiveRecord::Base.connection.instance_variable_get(:@config)[:database]}"
     puts cmd
     result = system(cmd)
@@ -9,6 +10,7 @@ class AddLtreeType < ActiveRecord::Migration
 
   def self.down
     execute "ALTER TABLE \"documents\" DROP \"path\""
+    #FIXME user require load from config
     cmd = "psql -Upostgres -f `pg_config --sharedir`/contrib/uninstall_ltree.sql #{ActiveRecord::Base.connection.instance_variable_get(:@config)[:database]}"
     puts cmd
     result = system(cmd)
