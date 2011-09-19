bf1577de452052f26eb4750dbbfc507e8e9908ea
dima <pronix.service@gmail.com>
2011-09-19 15:42:24 +0400

    fix user for load ltree

--- db/migrate/20110914103249_add_ltree_type.rb
+++ db/migrate/20110914103249_add_ltree_type.rb
@@ -1,6 +1,6 @@
 class AddLtreeType < ActiveRecord::Migration
   def self.up
-    cmd = "psql -f `pg_config --sharedir`/contrib/ltree.sql #{ActiveRecord::Base.connection.instance_variable_get(:@config)[:database]}"
+    cmd = "psql -Upostgres -f `pg_config --sharedir`/contrib/ltree.sql #{ActiveRecord::Base.connection.instance_variable_get(:@config)[:database]}"
     puts cmd
     result = system(cmd)
     raise "Bad exit" unless result
@@ -9,7 +9,7 @@ class AddLtreeType < ActiveRecord::Migration
 
   def self.down
     execute "ALTER TABLE \"documents\" DROP \"path\""
-    cmd = "psql -f `pg_config --sharedir`/contrib/uninstall_ltree.sql #{ActiveRecord::Base.connection.instance_variable_get(:@config)[:database]}"
+    cmd = "psql -Upostgres -f `pg_config --sharedir`/contrib/uninstall_ltree.sql #{ActiveRecord::Base.connection.instance_variable_get(:@config)[:database]}"
     puts cmd
     result = system(cmd)
     raise "Bad exit" unless result
