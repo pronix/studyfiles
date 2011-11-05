511b016d48b794165aa147f16a7481426310e7da
JD Frag <jdfrag@mail.ru>
2011-11-05 19:00:23 +0200

    стили, картинки и скрипты

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,29 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+  $(".sidebar .top-10 li:even").each(function(){
+    $(this).addClass("odd");
+  });
+  
+  //раскрыть список предметов
+  $('.slide-subjects').click(function(){
+    $(this).parent().next('.university-subjects-full').slideDown(400);
+	$(this).parent().next('.university-subjects-full').css('display', 'inline-block');
+	$(this).parent().css('display', 'none');
+        return false
+  });
+  //регистраций и авторизация модальным окном
+  $( "#registration-modal" ).dialog({
+	  autoOpen: false,
+	  closeText: '',
+	  position: 'top',
+	  width: 747,
+	  height: 467
+	});
+	$( ".sign-in-up a" ).click(function() {
+			$( "#registration-modal" ).dialog( "open" );
+			return false;
+	});
+	$( ".close-modal" ).click(function() {
+			$( "#registration-modal" ).dialog( "close" );
+			return false;
+	});
+});
