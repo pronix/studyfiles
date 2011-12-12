9a4a23e5b3ec3d2d0b79eb5daa7946b79b2bbe8b
dima <pronix.service@gmail.com>
2011-12-12 08:32:23 +0100

    files заготовочки

--- features/step_definitions/files.rb
+++ features/step_definitions/files.rb
@@ -1,29 +1,34 @@
 # -*- coding: utf-8 -*-
 
-Given /^в системе есть вузы$/ do 
+Given /^в системе есть вузы$/ do
   University.count.should be > 0
 end
 
-Given /^в системе есть файлы$/ do 
-  Factory.create(:document, 
+Given /^в системе есть файлы$/ do
+  Factory.create(:document,
+      :name => "КВопросы к экзамену.doc",
+      :item => File.new("#{Rails.root}/sample_documents/КВопросы к экзамену.doc"),
+      :folder_id => 904
+  )
+  Factory.create(:document,
       :name => "Конспект.doc",
       :item => File.new("#{Rails.root}/sample_documents/Конспект.doc"),
       :folder_id => 902
   )
-  
-  Factory.create(:document, 
+
+  Factory.create(:document,
       :name => "Документ 1",
       :item => File.new("#{Rails.root}/sample_documents/документ1.doc"),
       :folder_id => 902
   )
-  
-  Factory.create(:document, 
+
+  Factory.create(:document,
       :name => "Документ 3",
       :item => File.new("#{Rails.root}/sample_documents/документ3.doc"),
       :folder_id => 903
   )
-  
-  Factory.create(:document, 
+
+  Factory.create(:document,
       :name => "Длинные названия и описания сокращаются до коротких названий и описаний",
       :item => File.new("#{Rails.root}/sample_documents/hugo.doc"),
       :folder_id => 903
@@ -35,6 +40,12 @@ Given /^у пользователя "([^"]*)" нету файлов$/ do |email|
   user.documents.count.should be 0
 end
 
+# т.к. капчу гугловская - то в тестовом окружении она всегда корректна
+When /я должен ввести валидну капчу/ do
+  true
+end
 
-
-
+Then /^получу файл с именем "([^"]*)"/ do |file|
+  puts page.response_headers
+  page.response_headers['Content-Disposition'].should =~ /#{file}/
+end
