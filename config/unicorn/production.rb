1552fd2f0e546e51dedc5b6457fef34d884098bc
Maxim Kremenev <ezotrank@gmail.com>
2011-12-13 10:14:47 +0200

    unicorn config

---
+++ config/unicorn/production.rb
@@ -0,0 +1,13 @@
+app_path = Rails.root
+# Set unicorn options
+worker_processes 1
+preload_app true
+timeout 180
+listen "127.0.0.1:9000"
+
+# Log everything to one file
+stderr_path "log/unicorn.log"
+stdout_path "log/unicorn.log"
+
+# Set master PID location
+pid "#{app_path}/tmp/pids/unicorn.pid"
