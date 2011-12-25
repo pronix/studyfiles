41f8186564f56785159a4e682289ba55f0f4c727
Maxim Kremenev <ezotrank@gmail.com>
2011-12-25 19:38:40 +0200

    add files sort by rating as DESC and ASC

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,14 +1,29 @@
 class FilesController < ApplicationController
+  before_filter :sort_order
+  
   def index
     @universities = University.
       search(params[:search],
              :without => {:available => false},
-             :star => true, :page => params[:page],
-             :per_page => 15, :order => :rating, :sort_mode => :desc)
+             :star => true, :order => @order, :sort_mode => @sort_a)
   end
 
   def search
     @results = ThinkingSphinx.search params[:search],
     :classes => [Document, Folder]
+  end
+
+  private
+  def sort_order
+    if @sort = params[:sort]
+      if @sort == 'rating DESC'
+        @order = :rating
+        @sort_a = :desc
+      end
+    else
+      @sort = ''
+      @order = :rating
+      @sort_a = :desc
+    end
   end
 end
